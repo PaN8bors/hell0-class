9884a9792089c5b3af98f19940aeece207f49eaa
Parish Neighbors <pn8bors@gmail.com>
2022-05-04 12:34:47 -0500

    Second commit

--- Canvas Project/main.js
+++ Canvas Project/main.js
@@ -9,12 +9,26 @@ function save(picture) {
 function main() {
     const picture = canvas.createCanvas(400, 400);
     const ctx = picture.getContext('2d');
+    let gradient = ctx.createLinearGradient(0, 0, 300, 0);
 
-    ctx.fillStyle = 'white';
+    // Gradient filled rectangle
+    gradient.addColorStop(0, 'green');
+    gradient.addColorStop(.7, 'white');
+    gradient.addColorStop(1, 'pink');
+    ctx.fillStyle = gradient;
     ctx.fillRect(25, 25, 300, 100);
+
+    // Shadow
+    ctx.shadowColor = 'red';
+    ctx.shadowOffsetX = 10;
+    ctx.shadowOffsetY = 10;
+
+    // Font
     ctx.fillStyle = 'blue';
     ctx.font = '30px serif';
     ctx.fillText('Hello Class', 50, 50);
+
+
     
 
     save(picture);
